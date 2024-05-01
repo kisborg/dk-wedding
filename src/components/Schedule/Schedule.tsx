2bf2b85cae83e4f98f4c66174ebfa3f9f4e099ca
kisborg <mark.borgstrom@gmail.com>
2024-05-01 23:19:00 +0200

    schedule + styling fixes

--- src/components/Schedule/Schedule.tsx
+++ src/components/Schedule/Schedule.tsx
@@ -1,11 +1,16 @@
 import './Schedule.scss';
-import ScheduleImage from '../../../public/images/schedule.png';
+import ScheduleImageEn from '../../../public/images/schedule.png';
+import ScheduleImageHu from '../../../public/images/program_hu.png';
 import Image from 'next/image';
+import { useParams } from 'next/navigation';
 
 const Schedule = () => {
+  const params = useParams();
+  const scheduleImage =
+    params.locale === 'en' ? ScheduleImageEn : ScheduleImageHu;
   return (
     <div className='schedule'>
-      <Image className='schedule-image' alt='schedule' src={ScheduleImage} />
+      <Image className='schedule-image' alt='schedule' src={scheduleImage} />
     </div>
   );
 };
